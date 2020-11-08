c33c5539c92ab606ddb4fde2e5e5864244901ae4
Mikhail Kolyasnikov <mishak4ever@gmail.com>
2020-11-09 00:05:10 +0500

    fix ДЗ №6

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -58,7 +58,7 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
-    let addEventListener = target.addEventListener('click', function(event) {
+    target.addEventListener('click', function(event) {
         if(event.target.tagName === "BUTTON"){
             fn();
         }
